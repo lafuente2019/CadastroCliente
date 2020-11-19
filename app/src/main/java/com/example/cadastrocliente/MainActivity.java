2a0dbabb52a379af98df7ba10f3e0ff8b0dfc184
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-11-19 18:29:09 -0300

    implementação da MainActivity

--- app/src/main/java/com/example/cadastrocliente/MainActivity.java
+++ app/src/main/java/com/example/cadastrocliente/MainActivity.java
@@ -19,7 +19,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lista;
     ClienteDao dao;
+    ArrayList<Cliente> listCliente;
     Cliente cliente;
     ArrayAdapter adapter;
 
@@ -27,5 +29,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_Cadastar = (Button) findViewById(R.id.btnCadastrar);
+        btn_Cadastar.setOnClickListener(new android.view.View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FormularioCliente.class);
+                startActivity(intent);
+
+            }
+        });
+
+        lista = (ListView) findViewById(R.id.listCliente);
+        registerForContextMenu(lista);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+                Cliente esolhido = (Cliente) adapter.getItemAtPosition(position);
+
+                Intent i = new Intent(MainActivity.this, FormularioCliente.class);
+                i.putExtra("escolhido", esolhido);
+                startActivity(i);
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
+                cliente = (Cliente) adapter.getItemAtPosition(position);
+                return false;
+            }
+        });
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem menuDelete = menu.add("Deletar esse Cliente");
+        menuDelete.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                dao = new ClienteDao(MainActivity.this);
+                dao.excluir(cliente);
+                dao.close();
+                carregarCliente();
+                return true;
+            }
+        });
+    }
+
+    protected  void onResume(){
+        super.onResume();
+        carregarCliente();
+    }
+
+    public  void carregarCliente(){
+        dao = new ClienteDao(MainActivity.this);
+        listCliente = dao.getList();
+        dao.close();
+
+        if(listCliente != null){
+            adapter = new ArrayAdapter<Cliente>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, listCliente);
+            lista.setAdapter(adapter);
+        }
+
     }
 }
